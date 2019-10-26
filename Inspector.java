1f131dcb753721fe7e86082096b7530ee50c530c
Cristhian <csotelo88@hotmail.com>
2019-10-25 19:21:24 -0600

    Extrac Method: printMethods()

--- Inspector.java
+++ Inspector.java
@@ -37,51 +37,7 @@ public class Inspector {
           printConstructors(c, insideIndent);
         }
 
-        System.out.println(insideIndent + "METHODS: FROM: " + c.getName());
-        Method[] methods = c.getDeclaredMethods();
-        if (methods.length > 0) {
-          for (int i = 0; i < methods.length; i++) {
-            System.out.println(insideIndent + "\tMETHOD NAME: " + methods[i].getName() + " FROM " + c.getName());
-            Class[] exceptions = methods[i].getExceptionTypes();
-            if (exceptions.length > 0) {
-              System.out.print(insideIndent + "\tEXCEPTIONS THROWN: ");
-              for (int j = 0; j < exceptions.length; j++) {
-                System.out.print(exceptions[j].getName());
-                if (j == (exceptions.length - 1)) {
-                  System.out.println("");
-                } else {
-                  System.out.print(", ");
-                }
-              }
-
-            } else {
-              System.out.println(insideIndent + "\tEXCEPTIONS THROWN: NONE");
-            }
-
-            Class[] parameters = methods[i].getParameterTypes();
-            if (parameters.length > 0) {
-              System.out.print(insideIndent + "\tPARAMETERS: (");
-              for (int k = 0; k < parameters.length; k++) {
-                System.out.print(parameters[k].getName());
-                if (k == (parameters.length - 1)) {
-                  System.out.println(")");
-                } else {
-                  System.out.print(", ");
-                }
-              }
-            } else {
-              System.out.println(insideIndent + "\tPARAMETERS: NONE");
-            }
-
-            System.out.println(insideIndent + "\tRETURN TYPE: " + methods[i].getReturnType().getName());
-            System.out.println(insideIndent + "\tMODIFIER: " + Modifier.toString(methods[i].getModifiers()));
-
-            System.out.println(insideIndent + "\t-----------------");
-          }
-
-        } else {
-          System.out.println(insideIndent + "\tNONE");
-        }
+        printMethods(c, insideIndent);
 
         System.out.println(insideIndent + "FIELDS: FROM: " + c.getName());
         Field[] fields = c.getDeclaredFields();
@@ -175,6 +131,51 @@ public class Inspector {
             System.out.println(indent + "\tPARAMETERS: NONE");
           }
           System.out.println(indent + "\tMODIFIER: " + Modifier.toString(constructors[i].getModifiers()));
+          System.out.println(indent + "\t-----------------");
+        }
+      } else {
+        System.out.println(indent + "\tNONE");
+      }
+    }
+
+    private void printMethods(Class c, String indent) {
+      System.out.println(indent + "METHODS: FROM: " + c.getName());
+      Method[] methods = c.getDeclaredMethods();
+      if (methods.length > 0) {
+        for (int i = 0; i < methods.length; i++) {
+          System.out.println(indent + "\tMETHOD NAME: " + methods[i].getName() + " FROM " + c.getName());
+          Class[] exceptions = methods[i].getExceptionTypes();
+          if (exceptions.length > 0) {
+            System.out.print(indent + "\tEXCEPTIONS THROWN: ");
+            for (int j = 0; j < exceptions.length; j++) {
+              System.out.print(exceptions[j].getName());
+              if (j == (exceptions.length - 1)) {
+                System.out.println("");
+              } else {
+                System.out.print(", ");
+              }
+            }
+          } else {
+            System.out.println(indent + "\tEXCEPTIONS THROWN: NONE");
+          }
+
+          Class[] parameters = methods[i].getParameterTypes();
+          if (parameters.length > 0) {
+            System.out.print(indent + "\tPARAMETERS: (");
+            for (int k = 0; k < parameters.length; k++) {
+              System.out.print(parameters[k].getName());
+              if (k == (parameters.length - 1)) {
+                System.out.println(")");
+              } else {
+                System.out.print(", ");
+              }
+            }
+          } else {
+            System.out.println(indent + "\tPARAMETERS: NONE");
+          }
+
+          System.out.println(indent + "\tRETURN TYPE: " + methods[i].getReturnType().getName());
+          System.out.println(indent + "\tMODIFIER: " + Modifier.toString(methods[i].getModifiers()));
           System.out.println(indent + "\t-----------------");
         }
       } else {
